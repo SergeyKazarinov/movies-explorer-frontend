5d7b7bed4016beaaf53f1a8d2be7d72a29a2726a
Sergey Kazarinov <kazarinov092@gmail.com>
2022-11-18 11:48:26 +0500

    hotfix(portfolio): add list tag for projects

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -5,13 +5,13 @@ const Portfolio = () => {
   return(
     <section className="portfolio">
       <h2 className="portfolio__title">Портфолио</h2>
-      <div className="portfolio__flex">
-        <Project title="Статичный сайт" link="https://github.com/SergeyKazarinov/how-to-learn" />
-        <div className="portfolio__line"></div>
-        <Project title="Адаптивный сайт" link="https://github.com/SergeyKazarinov/russian-travel" />
-        <div className="portfolio__line"></div>
-        <Project title="Одностраничное приложение" link="https://github.com/SergeyKazarinov/react-mesto-api-full" />
-      </div>
+      <ul className="list portfolio__flex">
+        <li><Project title="Статичный сайт" link="https://github.com/SergeyKazarinov/how-to-learn" /></li>
+        <li className="portfolio__line"></li>
+        <li><Project title="Адаптивный сайт" link="https://github.com/SergeyKazarinov/russian-travel" /></li>
+        <li className="portfolio__line"></li>
+        <li><Project title="Одностраничное приложение" link="https://github.com/SergeyKazarinov/react-mesto-api-full" /></li>
+      </ul>
     </section>
   )
 }
