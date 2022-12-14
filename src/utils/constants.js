b3b533ff3b23602c7da7a25cc096abe891fda706
Sergey Kazarinov <kazarinov092@gmail.com>
2022-12-14 15:25:33 +0500

    refact(URL): change URL_API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,8 +23,8 @@ export const ERROR_INPUT_SEARCH_MOVIES_MESSAGE = "Нужно ввести клю
 export const EMAIL_PATTERN = '^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$';
 
 
-export const BASE_URL_FOR_MAIN_API = 'http://localhost:5000';
-// export const BASE_URL_FOR_MAIN_API = 'https://api.movies.kazarinov.nomoredomains.icu';
+// export const BASE_URL_FOR_MAIN_API = 'http://localhost:5000';
+export const BASE_URL_FOR_MAIN_API = 'https://api.movies.kazarinov.nomoredomains.icu';
 export const BASE_URL_FOR_MOVIE_IMAGES = 'https://api.nomoreparties.co';
 export const BASE_URL_FOR_MOVIES = 'https://api.nomoreparties.co/beatfilm-movies';
 
